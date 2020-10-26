800e0bd5ff8b669c06704f8162edbca0ca15bafd
melal <melal_dev_team@outlook.com>
2020-10-26 16:09:05 +0330

    using useState,useEffect in App

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,40 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Axios from "axios";
-import {IActivity} from "../models/activity";
+import { IActivity } from "../models/activity";
 import { Header, Icon, List } from "semantic-ui-react";
 
-interface IState{
-    activities : IActivity[],
-}
+const App = () => {
+    const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component<{},IState> {
-    readonly state:IState = {
-        activities: [],
-    };
-
-    componentDidMount() {
-        Axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
-            this.setState({
-                activities: response.data,
-            });
-        });
-    }
-    render() {
-        return (
-            <div>
-                <Header as="h2">
-                    <Icon name="users" />
-                    <Header.Content>Reactivities</Header.Content>
-                </Header>
-                <List>
-                    {this.state.activities.map((activity) => (
-                        <List.Item key={activity.id}>{activity.title}</List.Item>
-                    ))}
-                </List>
-            </div>
+    useEffect(() => {
+        Axios.get<IActivity[]>("http://localhost:5000/api/activities").then(
+            (response) => {
+                setActivities(response.data);
+            }
         );
-    }
-}
+    }, []);
+    // componentDidMount() {
+    //     Axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
+    //         this.setState({
+    //             activities: response.data,
+    //         });
+    //     });
+    // }
+
+    return (
+        <div>
+            <Header as="h2">
+                <Icon name="users" />
+                <Header.Content>Reactivities</Header.Content>
+            </Header>
+            <List>
+                {activities.map((activity) => (
+                    <List.Item key={activity.id}>{activity.title}</List.Item>
+                ))}
+            </List>
+        </div>
+    );
+};
 
 export default App;
