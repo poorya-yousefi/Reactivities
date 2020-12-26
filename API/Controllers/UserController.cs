b12565a4766db4ed8dcfd289dd89e859b2c15a51
Poorya <poorya3976@gmail.com>
2020-12-27 01:32:21 +0330

    End of section 12

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -6,16 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     public class UserController : BaseController
     {
         [HttpPost("login")]
+        [AllowAnonymous]
         public async Task<ActionResult<LoginModel>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
         }
 
         [HttpPost("register")]
+        [AllowAnonymous]
         public async Task<ActionResult<RegisterModel>> Register(Register.Command command)
         {
             return await Mediator.Send(command);
