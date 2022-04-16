981beee351e9ca7a5290e5878d46e5fc84b5ed34
Poorya <poorya3976@gmail.com>
2022-04-16 13:58:36 +0430

    End of section 14 - activity attendance added.

---
+++ Domain/ActivityAttendee.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string AppUserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
+        public Guid ActivityId { get; set; }
+        public virtual Activity Activity { get; set; }
+        public bool IsHost { get; set; }
+    }
+}
